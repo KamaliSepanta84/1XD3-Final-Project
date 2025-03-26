5fbe5092ebbf72b450bfb1d1822d21dcb0669d37
KamaliSepanta84 <reymondpalmer507@gmail.com>
2025-03-26 11:30:56 -0400

    Fixed frontend errors and improved login/sign up page

--- js/login.js
+++ js/login.js
@@ -1,15 +1,15 @@
 window.addEventListener("load", function(event){
-      const container = document.getElementById("container");
-      const overlayBtn = document.getElementById("overlayBtn");
-      const companyName = this.document.getElementById("title-container");
+  const container = document.getElementById("container");
+  const overlayBtn = document.getElementById("overlayBtn");
+  const companyName = this.document.getElementById("title-container");
 
-      overlayBtn.addEventListener("click", () => {
-        container.classList.toggle("right-panel-active");
-        companyName.classList.toggle("title-primary");
-        companyName.classList.toggle("title-white");
-        overlayBtn.classList.remove("scale-btn-animation");
-        window.requestAnimationFrame(() => {
-          overlayBtn.classList.add("scale-btn-animation");
-        });
-      });
+  overlayBtn.addEventListener("click", () => {
+    container.classList.toggle("right-panel-active");
+    companyName.classList.toggle("title-primary");
+    companyName.classList.toggle("title-white");
+    overlayBtn.classList.remove("scale-btn-animation");
+    window.requestAnimationFrame(() => {
+      overlayBtn.classList.add("scale-btn-animation");
+    });
+  });
 });
