0e425103ca17e9c347e797edd63d779051f4ab2c
KamaliSepanta84 <reymondpalmer507@gmail.com>
2025-04-22 00:49:46 -0400

    .

--- js/home.js
+++ js/home.js
@@ -1,28 +1,28 @@
 //HOME PAGE JS
-document.addEventListener("DOMContentLoaded", () => {
-    // CAROSEL CODE
-    //Set DOM elements and variables
-    let slides = document.querySelector('.slides');
-    let slideElements = document.querySelectorAll('.slide');
-    let totalSlides = slideElements.length;
-    let currentIndex = 0;
-    // Set the width of the slides container
-    function updateCarousel() {
-      slides.style.transform = 'translateX(' + (-currentIndex * 100) + '%)';
-    }
-    // Next button
-    document.querySelector('.next').addEventListener('click', () => {
-      currentIndex = (currentIndex + 1) % totalSlides;
-      updateCarousel();
-    });
-    // Prev button
-    document.querySelector('.prev').addEventListener('click', () => {
-      currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-      updateCarousel();
-    });
-    // Auto-play
-    setInterval(() => {
-      currentIndex = (currentIndex + 1) % totalSlides;
-      updateCarousel();
-    }, 4000);
-  });
+// document.addEventListener("DOMContentLoaded", () => {
+//     // CAROSEL CODE
+//     //Set DOM elements and variables
+//     let slides = document.querySelector('.slides');
+//     let slideElements = document.querySelectorAll('.slide');
+//     let totalSlides = slideElements.length;
+//     let currentIndex = 0;
+//     // Set the width of the slides container
+//     function updateCarousel() {
+//       slides.style.transform = 'translateX(' + (-currentIndex * 100) + '%)';
+//     }
+//     // Next button
+//     document.querySelector('.next').addEventListener('click', () => {
+//       currentIndex = (currentIndex + 1) % totalSlides;
+//       updateCarousel();
+//     });
+//     // Prev button
+//     document.querySelector('.prev').addEventListener('click', () => {
+//       currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+//       updateCarousel();
+//     });
+//     // Auto-play
+//     setInterval(() => {
+//       currentIndex = (currentIndex + 1) % totalSlides;
+//       updateCarousel();
+//     }, 4000);
+//   });
