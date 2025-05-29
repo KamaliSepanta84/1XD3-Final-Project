3bb5c78a6ab6d4079941d9eae8ceaad32efab72e
KamaliSepanta84 <reymondpalmer507@gmail.com>
2025-05-29 18:35:13 -0400

    fixes dome bugs

--- server/connect.php
+++ server/connect.php
@@ -5,16 +5,11 @@
  */
 try {
     $dbh = new PDO(
-<<<<<<< HEAD
-        "mysql:host=localhost;dbname=kosoricm_db", 
-        "root",
-        "" 
-=======
+
         "mysql:host=localhost;dbname=kamals19_db", 
         "root",
         ""
         //;>VdLnYe
->>>>>>> 749e01dcfc1df13c80fecbe257704acf92337c00
     );
 } catch (Exception $e) {
     die("ERROR: Couldn't connect. {$e->getMessage()}");
